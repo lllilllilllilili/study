c6cd030bb1e2567343df404043d41a65d4693099
lllilllilllilili <hik3562@hufs.ac.kr>
2023-02-11 05:29:09 +0900

    aop | com.studyaop.aop 아래의 패키지 안에 EventService 객체에 모든 메소드에 Aspect 적용한다.

---
+++ spring-boot/aop/src/main/java/com/studyaop/aop/config/PerfAspect.java
@@ -0,0 +1,19 @@
+package com.studyaop.aop.config;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class PerfAspect {
+
+    @Around(value = "execution(* com.studyaop.aop.*.EventService.*(..))")
+    public Object logPerf(ProceedingJoinPoint pjp) throws Throwable{
+        long begin = System.currentTimeMillis();
+        Object retVal = pjp.proceed(); // 메서드 호출 자체를 감쌈
+        System.out.println(System.currentTimeMillis() - begin);
+        return retVal;
+    }
+}
